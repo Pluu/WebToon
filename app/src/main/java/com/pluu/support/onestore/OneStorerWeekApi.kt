1878e70f546150aac5474e7064249bf944f1ed56
Pluu <pluulove@gmail.com>
2018-02-20 02:03:18 +0900

    원스토어 로직 수정
    - update #33

--- app/src/main/java/com/pluu/support/onestore/OneStorerWeekApi.kt
+++ app/src/main/java/com/pluu/support/onestore/OneStorerWeekApi.kt
@@ -1,72 +1,87 @@
 package com.pluu.support.onestore
 
 import android.content.Context
+import com.pluu.kotlin.asSequence
+import com.pluu.kotlin.toFormatString
 import com.pluu.support.impl.AbstractWeekApi
 import com.pluu.support.impl.NAV_ITEM
 import com.pluu.support.impl.REQUEST_METHOD
 import com.pluu.webtoon.item.Status
 import com.pluu.webtoon.item.WebToonInfo
-import org.jsoup.Jsoup
+import org.json.JSONArray
+import org.json.JSONObject
+import java.util.*
 
 /**
  * TStore 웹툰 Week Api
  * Created by pluu on 2017-04-27.
  */
 class OneStorerWeekApi(context: Context) : AbstractWeekApi(context, OneStorerWeekApi.TITLE) {
-    private val ID_PATTERN =
-        arrayOf("(?<=prodId=).+(?=&)".toRegex(), "(?<=goAdultAuthPage\\(').+(?=')".toRegex())
-
-    private var currentPos: Int = 0
+    private var currentPos = 0
+    private var page = 0
+    private var startKey: String? = null
 
     override val naviItem: NAV_ITEM = NAV_ITEM.ONE_STORE
 
     override fun parseMain(position: Int): List<WebToonInfo> {
-        currentPos = position
+        currentPos = position + 1
 
-        val doc = try {
-            Jsoup.parse(requestApi())
+        val array: JSONArray = try {
+            JSONArray().apply {
+                var hasNext: Boolean
+                do {
+                    val webtoonVO: JSONObject =
+                        JSONObject(requestApi()).optJSONObject("webtoonVO") ?: return@apply
+                    hasNext = webtoonVO.optString("hasNext") == "Y"
+                    startKey = webtoonVO.optString("startKey")
+
+                    webtoonVO.optJSONArray("webtoonList")
+                        ?.asSequence()
+                        ?.forEach {
+                            put(it)
+                        }
+                    page++
+                } while (hasNext)
+            }
         } catch (e: Exception) {
             e.printStackTrace()
             return emptyList()
         }
 
-        return doc.select(".offering-card-item")
-            .mapNotNull { element ->
-                val href = element.attr("href")
-                ID_PATTERN
-                    .mapNotNull { it.find(href) }
-                    .firstOrNull()?.value?.let { id ->
-                    WebToonInfo(id).apply {
-                        title = element.select(".product-ti").text()
-                        image = element.select(".thumbnail").attr("data-thumbackground")
-                        writer = element.select(".product-writer").text()
+        val currentDate = Date().toFormatString("yyyyMMdd")
 
-                        val related = element.select(".product-related")
-                        updateDate = related.last().text()
-                        status = when {
-                            element.select("em[class=.product-badge-icon product-badge-icon-01]").text() == "UP" -> Status.UPDATE
-                            related.first().text() == "휴재" -> Status.BREAK
-                            else -> Status.NONE
-                        }
-                    }
+        return array.asSequence().map {
+            WebToonInfo(it.optString("channelId")).apply {
+                title = it.optString("prodNm")
+                image = it.optString("filePos")
+                writer = it.optString("artistNm")
+                updateDate = it.optString("updateDate")
+                status = when (it.optString("statusCd")) {
+                    "continue" -> if (it.optString("updateDate") == currentDate) Status.UPDATE else Status.NONE
+                    "rest" -> Status.BREAK
+                    else -> Status.NONE
                 }
+                isAdult = it.optString("intentProdGrdCd") == "4"
             }
+        }.toList()
     }
 
     override val method: REQUEST_METHOD = REQUEST_METHOD.POST
 
     override val url: String
-        get() = "http://m.onestore.co.kr/mobilepoc/webtoon/weekdayList.omp"
-
-    override val headers: Map<String, String>
-        get() = hashMapOf(
-            "Referer" to "http://m.onestore.co.kr/mobilepoc/webtoon/weekdayList.omp"
-        )
+        get() = when (page) {
+            0 -> "http://m.onestore.co.kr/mobilepoc/webtoon/weekdayListDetail.omp"
+            else -> "http://m.onestore.co.kr/mobilepoc/webtoon/weekdayListMore.omp"
+        }
 
     override val params: Map<String, String>
         get() = hashMapOf(
             "weekday" to currentPos.toString()
-        )
+        ).apply {
+            startKey?.let {
+                "startKey" to it
+            }
+        }
 
     companion object {
         private val TITLE = arrayOf("월", "화", "수", "목", "금", "토", "T툰")
