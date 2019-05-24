6618ac3d337c2fbf640552e8836c764fe1972c19
pluu <pluulove@gmail.com>
2019-05-24 22:11:29 +0900

    Update Dependency
    - Add, ignore

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -1,65 +1,65 @@
 object Dep {
     object GradlePlugin {
-        val android = "com.android.tools.build:gradle:3.5.0-alpha13"
-        val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
-        val realm = "io.realm:realm-gradle-plugin:4.1.1"
-        val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:7.4.0"
+        const val android = "com.android.tools.build:gradle:3.6.0-alpha01"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
+        const val realm = "io.realm:realm-gradle-plugin:4.1.1"
+        const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:7.4.0"
     }
 
     object AndroidX {
-        val annotation = "androidx.annotation:annotation:1.1.0-beta01"
-        val activity = "androidx.activity:activity:1.0.0-alpha07"
-        val activityKtx = "androidx.activity:activity-ktx:1.0.0-alpha07"
-        val fragment = "androidx.fragment:fragment:1.1.0-alpha07"
-        val fragmentKtx = "androidx.fragment:fragment-ktx:1.1.0-alpha07"
-        val appcompat = "androidx.appcompat:appcompat:1.1.0-alpha04"
-        val coreKtx = "androidx.core:core-ktx:1.1.0-alpha05"
-        val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.1.0-alpha04"
+        const val annotation = "androidx.annotation:annotation:1.1.0-rc01"
+        const val activity = "androidx.activity:activity:1.0.0-alpha08"
+        const val activityKtx = "androidx.activity:activity-ktx:1.0.0-alpha08"
+        const val fragment = "androidx.fragment:fragment:1.1.0-alpha09"
+        const val fragmentKtx = "androidx.fragment:fragment-ktx:1.1.0-alpha09"
+        const val appcompat = "androidx.appcompat:appcompat:1.1.0-alpha05"
+        const val coreKtx = "androidx.core:core-ktx:1.2.0-alpha01"
+        const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.2.0-alpha01"
 
         object UI {
-            val recyclerview = "androidx.recyclerview:recyclerview:1.1.0-alpha04"
-            val palette = "androidx.palette:palette:1.0.0"
-            val cardview = "androidx.cardview:cardview:1.0.0"
-            val preference = "androidx.preference:preference:1.1.0-alpha04"
-            val browser = "androidx.browser:browser:1.0.0"
-            val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha5"
-            val material = "com.google.android.material:material:1.1.0-alpha05"
+            const val recyclerview = "androidx.recyclerview:recyclerview:1.1.0-alpha05"
+            const val palette = "androidx.palette:palette:1.0.0"
+            const val cardview = "androidx.cardview:cardview:1.0.0"
+            const val preference = "androidx.preference:preference:1.1.0-alpha05"
+            const val browser = "androidx.browser:browser:1.0.0"
+            const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta1"
+            const val material = "com.google.android.material:material:1.1.0-alpha06"
         }
     }
 
     object Kotlin {
-        val version = "1.3.31"
-        val stdlibJvm = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val version = "1.3.31"
+        const val stdlibJvm = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
 
-        private val coroutinesVersion = "1.2.1"
-        val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
-        val coroutinesAndroid =
+        private const val coroutinesVersion = "1.2.1"
+        const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
+        const val coroutinesAndroid =
             "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
     }
 
     object Koin {
-        private val version = "1.0.2"
-        val android = "org.koin:koin-android:$version"
-        val androidViewModel = "org.koin:koin-android-viewmodel:$version"
+        private const val version = "1.0.2"
+        const val android = "org.koin:koin-android:$version"
+        const val androidViewModel = "org.koin:koin-android-viewmodel:$version"
     }
 
-    val jsoup = "org.jsoup:jsoup:1.11.3"
+    const val jsoup = "org.jsoup:jsoup:1.12.1"
 
     object Glide {
-        private val version = "4.9.0"
-        val core = "com.github.bumptech.glide:glide:$version"
-        val compiler = "com.github.bumptech.glide:compiler:$version"
+        private const val version = "4.9.0"
+        const val core = "com.github.bumptech.glide:glide:$version"
+        const val compiler = "com.github.bumptech.glide:compiler:$version"
     }
 
     object OkHttp {
-        private val version = "3.14.1"
-        val core = "com.squareup.okhttp3:okhttp:$version"
-        val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
+        private const val version = "3.14.2"
+        const val core = "com.squareup.okhttp3:okhttp:$version"
+        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
     }
 
     object Test {
-        val junit = "junit:junit:4.12"
-        val assertJ = "org.assertj:assertj-core:3.11.1"
-        val mockito = "org.mockito:mockito-core:2.23.0"
+        const val junit = "junit:junit:4.12"
+        const val assertJ = "org.assertj:assertj-core:3.11.1"
+        const val mockito = "org.mockito:mockito-core:2.23.0"
     }
 }
