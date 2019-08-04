2ae8c10e3cac4ed22724b2581c56d8725cd0796e
pluu <pluulove@gmail.com>
2019-08-04 23:59:54 +0900

    Iterable Coroutine 처리

---
+++ app/src/main/java/com/pluu/webtoon/utils/coroutines/mapOnSuspend.kt
@@ -0,0 +1,10 @@
+package com.pluu.webtoon.utils.coroutines
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+
+// 참고 1. https://github.com/Kotlin/coroutines-examples/blob/master/examples/suspendingSequence/suspendingSequence.kt
+// 참고 2. https://jivimberg.io/blog/2018/05/04/parallel-map-in-kotlin/
+suspend fun <A, B> Iterable<A>.mapOnSuspend(f: suspend (A) -> B): List<B> = coroutineScope {
+    map { async { f(it) } }.map { it.await() }
+}
