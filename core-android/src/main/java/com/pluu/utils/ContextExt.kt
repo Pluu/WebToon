e2976adc9c6cbd2abf8cd753f05605f8d7653baf
pluu <pluulove@gmail.com>
2020-06-23 00:36:21 +0900

    Context KTX 추가

---
+++ core-android/src/main/java/com/pluu/utils/ContextExt.kt
@@ -0,0 +1,18 @@
+package com.pluu.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.core.os.bundleOf
+
+inline fun <reified T : Activity> Context.buildIntent(
+    vararg argument: Pair<String, Any?>
+) = Intent(this, T::class.java).apply {
+    putExtras(bundleOf(*argument))
+}
+
+inline fun <reified T : Activity> Context.startActivity(
+    vararg argument: Pair<String, Any?>
+) {
+    startActivity(buildIntent<T>(*argument))
+}
