06757c5a96d646d5edfe42f18b5d16e477739d21
pluu <pluulove@gmail.com>
2020-08-18 23:09:36 +0900

    Update Coroutine 1.3.9

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -85,7 +85,7 @@ object Dep {
         const val stdlibJvm = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
 
         object coroutines {
-            private const val coroutinesVersion = "1.3.8"
+            private const val coroutinesVersion = "1.3.9"
             const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
             const val android =
                 "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
