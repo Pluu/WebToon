05df75716521d495e5627a84c8508f877a3662f7
pluu <pluulove@gmail.com>
2020-08-27 01:26:10 +0900

    FragmentResultListener API 수정

--- core-android/src/main/java/com/pluu/utils/result/FragmentResultOwner.kt
+++ core-android/src/main/java/com/pluu/utils/result/FragmentResultOwner.kt
@@ -2,11 +2,12 @@ package com.pluu.utils.result
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.setFragmentResultListener
 
 fun FragmentActivity.setFragmentResultListener(
     requestKey: String,
     listener: (resultKey: String, bundle: Bundle) -> Unit
 ) {
-    supportFragmentManager.setFragmentResultListener(requestKey, this, listener)
+    supportFragmentManager.setFragmentResultListener(requestKey, this,
+        { _requestKey, bundle -> listener.invoke(_requestKey, bundle) }
+    )
 }
