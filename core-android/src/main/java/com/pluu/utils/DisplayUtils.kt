1cb5c9e113f4a0f0cc6e4af30b1c632adca876d5
pluu <pluulove@gmail.com>
2020-08-27 01:44:10 +0900

    Display 처리 수정

--- core-android/src/main/java/com/pluu/utils/DisplayUtils.kt
+++ core-android/src/main/java/com/pluu/utils/DisplayUtils.kt
@@ -4,6 +4,7 @@ import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
 import android.app.Activity
 import android.os.Build
+import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
 import android.view.WindowManager
 import android.view.animation.DecelerateInterpolator
 import android.view.animation.Interpolator
@@ -23,13 +24,19 @@ fun animatorColor(
 }
 
 fun Activity.setStatusBarColor(color: Int) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val controller = window.decorView.windowInsetsController ?: return
+        controller.setSystemBarsAppearance(
+            0,
+            APPEARANCE_LIGHT_STATUS_BARS
+        )
+    } else {
         window.apply {
             addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            statusBarColor = color
         }
     }
+    window.statusBarColor = color
 }
 
 inline fun Activity.animatorColorFromThemeAttrId(
