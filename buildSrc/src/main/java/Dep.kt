b16724ec0128f102ea6b5cf2c0bad644934cce5f
pluu <pluulove@gmail.com>
2020-09-11 21:13:28 +0900

    Update Dagger 2.29.1

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -63,13 +63,13 @@ object Dep {
     }
 
     object Dagger {
-        private const val version = "2.28.3"
+        private const val version = "2.29.1"
         const val dagger = "com.google.dagger:dagger:$version"
         const val compiler = "com.google.dagger:dagger-compiler:$version"
     }
 
     object Hilt {
-        const val version = "2.28.3-alpha"
+        const val version = "2.29-alpha"
         const val android = "com.google.dagger:hilt-android:${version}"
         const val hilt_compiler = "com.google.dagger:hilt-android-compiler:${version}"
         const val testing = "com.google.dagger:hilt-android-testing:${version}"
