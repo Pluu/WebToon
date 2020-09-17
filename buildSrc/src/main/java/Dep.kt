94b943d0796901d837187301c1b503629b6c907e
pluu <pluulove@gmail.com>
2020-09-17 23:09:34 +0900

    Update core-ktx 1.5.0-alpha03

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -21,7 +21,7 @@ object Dep {
         }
 
         const val appcompat = "androidx.appcompat:appcompat:1.3.0-alpha02"
-        const val coreKtx = "androidx.core:core-ktx:1.5.0-alpha02"
+        const val coreKtx = "androidx.core:core-ktx:1.5.0-alpha03"
 
         object fragment {
             private const val fragmentVersion = "1.3.0-alpha08"
