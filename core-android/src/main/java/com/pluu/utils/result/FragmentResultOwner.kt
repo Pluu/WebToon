3e2b429ab3643d7bd5dd8f07fe55d1b970f79919
pluu <pluulove@gmail.com>
2020-10-24 15:18:12 +0900

    Add, Fragment.setFragmentResult KTX 추가

--- core-android/src/main/java/com/pluu/utils/result/FragmentResultOwner.kt
+++ core-android/src/main/java/com/pluu/utils/result/FragmentResultOwner.kt
@@ -1,6 +1,7 @@
 package com.pluu.utils.result
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 
 fun FragmentActivity.setFragmentResultListener(
@@ -11,3 +12,8 @@ fun FragmentActivity.setFragmentResultListener(
         { _requestKey, bundle -> listener.invoke(_requestKey, bundle) }
     )
 }
+
+fun Fragment.setFragmentResult(
+    requestKey: String,
+    result: Bundle = Bundle()
+) = parentFragmentManager.setFragmentResult(requestKey, result)
