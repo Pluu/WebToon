6cb4577dea6824f1b539c73d5842790eac8bebc7
pluu <pluulove@gmail.com>
2020-11-11 00:57:02 +0900

    Flow to LiveData 처리로 로직 개선

--- app/src/main/java/com/pluu/webtoon/ui/intro/IntroViewModel.kt
+++ app/src/main/java/com/pluu/webtoon/ui/intro/IntroViewModel.kt
@@ -1,31 +1,15 @@
 package com.pluu.webtoon.ui.intro
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.util.concurrent.TimeUnit
+import kotlinx.coroutines.flow.flow
 
-/**
- * Intro ViewModel
- */
+/** Intro ViewModel */
 class IntroViewModel @ViewModelInject constructor() : ViewModel() {
-
-    private val _observe = MutableLiveData<Unit>()
-    val observe: LiveData<Unit>
-        get() = _observe
-
-    init {
-        viewModelScope.launch {
-            initAction()
-        }
-    }
-
-    private suspend fun initAction() {
-        delay(TimeUnit.SECONDS.toMillis(1L))
-        _observe.value = Unit
-    }
+    val observe = flow {
+        delay(1000L)
+        emit(Unit)
+    }.asLiveData()
 }
