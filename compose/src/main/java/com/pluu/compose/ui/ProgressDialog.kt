71dd39dfd10dfb561cb7610cafd0fbd6857219da
pluu <pluulove@gmail.com>
2020-11-21 01:20:30 +0900

    Add, ProgressDialog

---
+++ compose/src/main/java/com/pluu/compose/ui/ProgressDialog.kt
@@ -0,0 +1,85 @@
+package com.pluu.compose.ui
+
+import androidx.compose.foundation.ProvideTextStyle
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.material.AmbientEmphasisLevels
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideEmphasis
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
+@Composable
+fun ProgressDialog(
+    title: String? = null,
+    modifier: Modifier = Modifier.padding(12.dp),
+    onDismiss: () -> Unit = { },
+    shape: Shape = MaterialTheme.shapes.medium,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    contentColor: Color = contentColorFor(backgroundColor),
+    properties: DialogProperties? = null
+) {
+    dialogContent(
+        title = {
+            if (title != null) {
+                Text(text = title)
+            } else {
+                null
+            }
+        },
+        modifier = modifier,
+        onDismiss = onDismiss,
+        shape = shape,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        properties = properties
+    )
+}
+
+@Composable
+private fun dialogContent(
+    title: (@Composable () -> Unit)? = null,
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit,
+    shape: Shape = MaterialTheme.shapes.medium,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    contentColor: Color = contentColorFor(backgroundColor),
+    properties: DialogProperties? = null
+) {
+    val emphasisLevels = AmbientEmphasisLevels.current
+    Dialog(onDismissRequest = onDismiss, properties = properties) {
+        Surface(
+            modifier = modifier,
+            shape = shape,
+            color = backgroundColor,
+            contentColor = contentColor
+        ) {
+            Row {
+                CircularProgressIndicator(
+                    modifier = modifier.preferredSize(48.dp),
+                    color = MaterialTheme.colors.secondary
+                )
+                if (title != null) {
+                    Box(modifier = TitlePadding.align(Alignment.CenterVertically)) {
+                        ProvideEmphasis(emphasisLevels.high) {
+                            val textStyle = MaterialTheme.typography.subtitle1
+                            ProvideTextStyle(textStyle, title)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
