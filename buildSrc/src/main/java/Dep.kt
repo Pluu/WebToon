014f98feb2775f89628ef5cc5905052b783f9742
pluu <pluulove@gmail.com>
2021-03-23 01:23:28 +0900

    Update, AS Arctic Fox 11

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -2,7 +2,7 @@
 
 object Dep {
     object GradlePlugin {
-        const val androidStudioGradlePluginVersion = "7.0.0-alpha10"
+        const val androidStudioGradlePluginVersion = "7.0.0-alpha11"
         const val android = "com.android.tools.build:gradle:$androidStudioGradlePluginVersion"
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
         const val kotlinSerialization =
