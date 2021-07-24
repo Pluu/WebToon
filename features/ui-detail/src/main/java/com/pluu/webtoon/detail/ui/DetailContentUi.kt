a6548cfa33b831a12cce78c9882ce2ea08e211d9
pluu <pluulove@gmail.com>
2021-07-24 16:18:48 +0900

    Cached AdjustView bounds

--- features/ui-detail/src/main/java/com/pluu/webtoon/detail/ui/DetailContentUi.kt
+++ features/ui-detail/src/main/java/com/pluu/webtoon/detail/ui/DetailContentUi.kt
@@ -2,20 +2,28 @@ package com.pluu.webtoon.detail.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
 import com.google.accompanist.insets.navigationBarsHeight
 import com.google.accompanist.insets.statusBarsHeight
-import com.pluu.webtoon.detail.R
 import com.pluu.webtoon.model.DetailView
+import com.pluu.webtoon.utils.applyAgent
 
 @Composable
 fun DetailContentUi(
@@ -23,41 +31,70 @@ fun DetailContentUi(
     items: List<DetailView>,
     onClick: () -> Unit
 ) {
-    LazyColumn(modifier = modifier
-        .pointerInput(Unit) {
-            detectTapGestures(onTap = { onClick() })
-        }
-    ) {
-        itemsIndexed(items,
-            key = { _, item -> item.url }
-        ) { index, item ->
-            if (index == 0) {
-                Spacer(Modifier.statusBarsHeight(48.dp))
-            }
-            // TODO: Adjust Image 처리 개선 해야함
-            DetailPhotoView(item)
+    val cachedRatioMap = remember { mutableStateMapOf<String, Float>() }
 
-            if (items.size - 1 == index) {
-                Spacer(Modifier.navigationBarsHeight(48.dp))
+    BoxWithConstraints {
+        LazyColumn(modifier = modifier
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = { onClick() })
+            }
+        ) {
+            itemsIndexed(items,
+                key = { _, item -> item.url }
+            ) { index, item ->
+                if (index == 0) {
+                    Spacer(Modifier.statusBarsHeight(48.dp))
+                }
+
+                AdjustDetailImage(
+                    item = item,
+                    modifier = if (cachedRatioMap.contains(item.url)) {
+                        Modifier.aspectRatio(cachedRatioMap.getValue(item.url))
+                    } else {
+                        Modifier.size(maxWidth, maxHeight)
+                    }
+                ) { detailItem, size ->
+                    cachedRatioMap[detailItem.url] = size.width / size.height
+                }
+
+                if (items.size - 1 == index) {
+                    Spacer(Modifier.navigationBarsHeight(48.dp))
+                }
             }
         }
     }
 }
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
-private fun DetailPhotoView(
-    item: DetailView
+private fun AdjustDetailImage(
+    item: DetailView,
+    modifier: Modifier = Modifier,
+    onSuccess: (DetailView, Size) -> Unit
 ) {
+    val painter = rememberImagePainter(
+        data = item.url,
+        builder = {
+            applyAgent()
+            crossfade(true)
+        }
+    )
+
+    when (val state = painter.state) {
+        is ImagePainter.State.Success -> {
+            onSuccess(item, state.painter.intrinsicSize)
+        }
+        is ImagePainter.State.Loading -> {
+            CircularProgressIndicator(
+                modifier = modifier.size(60.dp),
+                color = MaterialTheme.colors.secondary
+            )
+        }
+    }
+
     Image(
-        painter = rememberImagePainter(
-            data = item.url,
-            builder = {
-                crossfade(true)
-                placeholder(R.drawable.ic_sentiment_very_dissatisfied_48)
-            },
-        ),
-        modifier = Modifier.fillMaxWidth(),
-        contentDescription = null,
-        contentScale = ContentScale.FillWidth
+        modifier = modifier,
+        painter = painter,
+        contentDescription = null
     )
 }
