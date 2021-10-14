1c4331f949d062a8d5e5084f9ef64c7e70bbb500
pluulove <pluulove@gmail.com>
2021-10-14 23:12:00 +0900

    Update, AS Arctic Fox path 3

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -2,7 +2,7 @@
 
 object Dep {
     object GradlePlugin {
-        const val androidStudioGradlePluginVersion = "7.0.2"
+        const val androidStudioGradlePluginVersion = "7.0.3"
         const val android = "com.android.tools.build:gradle:$androidStudioGradlePluginVersion"
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
         const val kotlinSerialization = "org.jetbrains.kotlin:kotlin-serialization:${Kotlin.version}"
