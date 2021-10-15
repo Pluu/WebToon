bab05b5bd04f87acd228ad2578e0a3428a2383c5
pluulove <pluulove@gmail.com>
2021-10-16 01:04:03 +0900

    ksp kotlin 버전 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
         google()
     }
     plugins {
-        id("com.google.devtools.ksp") version "1.5.30-1.0.0"
+        id("com.google.devtools.ksp") version "1.5.31-1.0.0"
     }
 }
 
