29cc827f1666806e840086c1e7421bdbdead192e
pluulove <pluulove@gmail.com>
2021-10-16 15:00:59 +0900

    root build.gradle 의 속성을 settings.gradle로 이관

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://plugins.gradle.org/m2/") {
-            content {
-                includeGroup("org.jlleitschuh.gradle")
-            }
-        }
-    }
-    dependencies {
-        classpath(Dep.GradlePlugin.android)
-        classpath(Dep.GradlePlugin.kotlin)
-        classpath(Dep.GradlePlugin.kotlinSerialization)
-        classpath(Dep.GradlePlugin.ktlint)
-        classpath(Dep.GradlePlugin.hilt)
-    }
-}
-
 task("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,27 @@
 pluginManagement {
+    val agpVersion = "7.0.3"
+    val kotlinVersion = "1.5.31"
+
     repositories {
         gradlePluginPortal()
         google()
+        mavenCentral()
     }
     plugins {
-        id("com.google.devtools.ksp") version "1.5.31-1.0.0"
+        id("com.android.application") version agpVersion
+        id("com.android.library") version agpVersion
+        id("org.jetbrains.kotlin.android") version kotlinVersion
+        id("org.jetbrains.kotlin.jvm") version kotlinVersion
+        id("org.jetbrains.kotlin.kapt") version kotlinVersion
+        id("com.google.devtools.ksp") version "${kotlinVersion}-1.0.0"
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (val id = requested.id.id) {
+                "dagger.hilt.android.plugin" -> useModule("com.google.dagger:hilt-android-gradle-plugin:2.39.1")
+                "org.jetbrains.kotlin.plugin.serialization" -> useModule("${id}:${id}.gradle.plugin:${kotlinVersion}")
+            }
+        }
     }
 }
 
@@ -13,7 +30,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://plugins.gradle.org/m2/")
+        maven("https://plugins.gradle.org/m2/") {
+            content {
+                includeGroup("org.jlleitschuh.gradle")
+            }
+        }
     }
 }
 
