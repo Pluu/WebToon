14342a0109b9d08877187f5cfe5c81f04aff5588
pluu <pluulove@gmail.com>
2022-01-14 16:57:41 +0900

    Update, Dagger의 javapoet 처리를 외부로 대응

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,4 +10,5 @@ repositories {
 dependencies {
     implementation("com.android.tools.build:gradle:7.0.4")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    implementation("com.squareup:javapoet:1.13.0")
 }
