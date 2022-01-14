deca82cc10a4c6dbcd680c72fed1161aa44cdda0
pluu <pluulove@gmail.com>
2022-01-14 23:53:37 +0900

    미사용 코드 제거

--- core-android/src/main/java/com/pluu/utils/AttributeExt.kt
+++ core-android/src/main/java/com/pluu/utils/AttributeExt.kt
@@ -2,19 +2,9 @@ package com.pluu.utils
 
 import android.content.Context
 import android.graphics.Color
-import android.util.TypedValue
 import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
 import androidx.core.content.res.use
-import androidx.fragment.app.Fragment
-
-fun Context.resolveAttribute(@AttrRes attributeResId: Int): TypedValue {
-    val typedValue = TypedValue()
-    if (theme.resolveAttribute(attributeResId, typedValue, true)) {
-        return typedValue
-    }
-    throw IllegalArgumentException(resources.getResourceName(attributeResId))
-}
 
 @ColorInt
 fun Context.getThemeColor(
@@ -23,8 +13,4 @@ fun Context.getThemeColor(
     intArrayOf(themeAttrId)
 ).use {
     it.getColor(0, Color.MAGENTA)
-}
-
-fun Fragment.resolveAttribute(@AttrRes attributeResId: Int): TypedValue {
-    return requireContext().resolveAttribute(attributeResId)
 }
