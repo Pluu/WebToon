437fac56fc59e80069f9d8da939e17a8ebb717a4
pluu <pluulove@gmail.com>
2022-01-28 22:58:05 +0900

    IntroViewModel에 HiltViewModel Annotation 적용

--- features/ui-intro/src/main/java/com/pluu/webtoon/intro/ui/IntroViewModel.kt
+++ features/ui-intro/src/main/java/com/pluu/webtoon/intro/ui/IntroViewModel.kt
@@ -2,6 +2,7 @@ package com.pluu.webtoon.intro.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -9,6 +10,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /** Intro ViewModel */
+@HiltViewModel
 class IntroViewModel @Inject constructor() : ViewModel() {
     private val _initLoading = MutableStateFlow(false)
     val observe: StateFlow<Boolean> get() = _initLoading
