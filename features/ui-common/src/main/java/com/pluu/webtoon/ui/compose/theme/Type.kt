4d77e1678fdeb8c8bee5e0b9f56c1616363bc84d
pluu <pluulove@gmail.com>
2022-01-31 23:41:07 +0900

    미사용 기능 제거 / Lint 반영

--- features/ui-common/src/main/java/com/pluu/webtoon/ui/compose/theme/Type.kt
+++ features/ui-common/src/main/java/com/pluu/webtoon/ui/compose/theme/Type.kt
@@ -15,7 +15,7 @@ val AppTypography = Typography(
 		fontWeight = FontWeight.W400,
 		fontSize = 57.sp,
 		lineHeight = 64.sp,
-		letterSpacing = -0.25.sp,
+		letterSpacing = -(0.25.sp),
 	),
 	displayMedium = TextStyle(
 		fontFamily = Roboto,
