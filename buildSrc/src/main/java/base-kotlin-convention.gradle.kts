9ef3e64238c11810463adcbe96b278800162f408
pluu <pluulove@gmail.com>
2022-02-06 15:26:34 +0900

    주석화 처리한 Kotlin Option 제거

--- buildSrc/src/main/java/base-kotlin-convention.gradle.kts
+++ buildSrc/src/main/java/base-kotlin-convention.gradle.kts
@@ -3,12 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = freeCompilerArgs + listOf(
-//          "-Xallow-jvm-ir-dependencies",
             "-Xskip-prerelease-check",
             "-Xopt-in=kotlin.RequiresOptIn",
-            // Enable experimental coroutines APIs, including Flow
-//          "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-//          "-Xopt-in=kotlinx.coroutines.FlowPreview",
             "-Xopt-in=kotlin.Experimental",
             "-Xjvm-default=enable"
         )
