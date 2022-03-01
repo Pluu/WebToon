8cb174dcb3991425bd379e366c9fef65e8578581
pluu <pluulove@gmail.com>
2022-03-01 15:19:25 +0900

    Update, Android Studio Chipmunk beta 03

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
-    val agpVersion = "7.2.0-beta02"
+    val agpVersion = "7.2.0-beta03"
     val kotlinVersion = "1.6.10"
 
     repositories {
