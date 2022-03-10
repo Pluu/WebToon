cb2953061735fe5becd8b104c5ce1855e369d6f0
pluu <pluulove@gmail.com>
2022-03-10 21:44:48 +0900

    Update, Android Studio Chipmunk beta 04

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.2.0-beta03")
+    implementation("com.android.tools.build:gradle:7.2.0-beta04")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     implementation("com.squareup:javapoet:1.13.0")
 }
