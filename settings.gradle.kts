b0cb718fcef0244accaf741a816491f8a363c12b
pluu <pluulove@gmail.com>
2022-06-17 22:46:35 +0900

    Dagger Plugin version 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -40,7 +40,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             when (val id = requested.id.id) {
-                "dagger.hilt.android.plugin" -> useModule("com.google.dagger:hilt-android-gradle-plugin:2.41")
+                "dagger.hilt.android.plugin" -> useModule("com.google.dagger:hilt-android-gradle-plugin:2.42")
                 "org.jetbrains.kotlin.plugin.serialization" -> useModule("${id}:${id}.gradle.plugin:${kotlinVersion}")
             }
         }
