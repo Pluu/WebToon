57f4fac377e6ad72c0bba5e882db000a5e951ae2
pluu <pluulove@gmail.com>
2022-06-23 21:57:34 +0900

    Update, Android Studio Dolphin beta 4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
-    val agpVersion = "7.3.0-beta03"
+    val agpVersion = "7.3.0-beta04"
     val kotlinVersion = "1.6.21"
 
     repositories {
