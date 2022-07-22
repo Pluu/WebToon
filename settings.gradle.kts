1d0a331d0d58505f5cc00357c367aa9334e47a48
pluu <pluulove@gmail.com>
2022-07-22 23:32:51 +0900

    Update, Kotlin/Compose version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     val agpVersion = "7.3.0-beta05"
-    val kotlinVersion = "1.7.0"
+    val kotlinVersion = "1.7.10"
 
     repositories {
         gradlePluginPortal()
