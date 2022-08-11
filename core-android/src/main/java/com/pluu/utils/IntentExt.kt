596612626fdd1a03fb8fc8c4bbb48ea345d508eb
pluu <pluulove@gmail.com>
2022-08-11 16:12:45 +0900

    Update Intent logic (support apilevel 33)

--- core-android/src/main/java/com/pluu/utils/IntentExt.kt
+++ core-android/src/main/java/com/pluu/utils/IntentExt.kt
@@ -1,21 +1,39 @@
 package com.pluu.utils
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import java.io.Serializable
 
-inline fun <reified T : Serializable> Intent.getRequiredSerializableExtra(name: String): T =
-    getSerializable(name) ?: throw IllegalArgumentException("$name does not reference")
+inline fun <reified T : Serializable> Intent.getRequiredSerializable(
+    name: String
+): T = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    requireNotNull(getSerializableExtra(name, T::class.java))
+} else {
+    getSerializableExtra(name) as T
+}
 
-inline fun <reified T : Serializable> Intent.getSerializable(name: String): T? =
-    getSerializableExtra(name) as? T
+inline fun <reified T : Parcelable> Intent.getRequiredParcelableExtra(
+    name: String
+): T = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    requireNotNull(getParcelableExtra(name, T::class.java))
+} else {
+    requireNotNull(getParcelableExtra(name)) as T
+}
 
-inline fun <reified T : Parcelable> Intent.getRequiredParcelableExtra(name: String): T =
-    getParcelableExtra(name) ?: throw IllegalArgumentException("$name does not reference")
+inline fun <reified T : Serializable> Bundle.getRequiredSerializableExtra(
+    name: String
+): T = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    requireNotNull(getSerializable(name, T::class.java))
+} else {
+    getSerializable(name) as T
+}
 
-inline fun <reified T : Serializable> Bundle.getRequiredSerializableExtra(name: String): T =
-    getSerializable(name) as? T ?: throw IllegalArgumentException("$name does not reference")
-
-inline fun <reified T : Parcelable> Bundle.getRequiredParcelableExtra(name: String): T =
-    getParcelable(name) ?: throw IllegalArgumentException("$name does not reference")
+inline fun <reified T : Parcelable> Bundle.getRequiredParcelableExtra(
+    name: String
+): T = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    requireNotNull(getParcelable(name, T::class.java))
+} else {
+    requireNotNull(getParcelable(name)) as T
+}
