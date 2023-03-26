b429cb7030410e19a3ef8fb737923624d8c330a5
pluu <pluulove@gmail.com>
2023-03-26 22:18:25 +0900

    New Project시에 작성되는 기본 정의 대응

--- ui-theme/src/main/java/com/pluu/webtoon/ui/compose/theme/Theme.kt
+++ ui-theme/src/main/java/com/pluu/webtoon/ui/compose/theme/Theme.kt
@@ -1,10 +1,15 @@
 package com.pluu.webtoon.ui.compose.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightThemeColors = lightColorScheme(
 
@@ -68,14 +73,22 @@ fun AppTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (!useDarkTheme) {
+    val colorScheme = if (!useDarkTheme) {
         LightThemeColors
     } else {
         DarkThemeColors
     }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = useDarkTheme
+        }
+    }
 
     MaterialTheme(
-        colorScheme = colors,
+        colorScheme = colorScheme,
         typography = AppTypography,
         content = content
     )
