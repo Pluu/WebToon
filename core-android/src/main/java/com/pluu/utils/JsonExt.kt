6047a8e26c7850b28809e16eed8e577b7c1f94f4
pluu <pluulove@gmail.com>
2023-04-22 15:20:29 +0900

    Json 관련 Extension 을 하나의 파일로 합침

--- core-android/src/main/java/com/pluu/utils/JsonExt.kt
+++ core-android/src/main/java/com/pluu/utils/JsonExt.kt
@@ -1,10 +1,24 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package com.pluu.utils
 
 import org.json.JSONArray
 import org.json.JSONObject
 
+inline fun JSONObject?.orEmpty(): JSONObject = this ?: JSONObject()
+
+inline fun JSONArray?.orEmpty(): JSONArray = this ?: JSONArray()
+
+inline fun <R> JSONObject.mapEach(transform: (String) -> R): List<R> {
+    val result = ArrayList<R>(length())
+    keys().forEach { key ->
+        result.add(transform(getString(key)))
+    }
+    return result
+}
+
 /**
- * Custom Extends
+ * Custom Json Array Extends
  * Created by pluu on 2017-04-17.
  */
 operator fun JSONArray.iterator(): Iterator<JSONObject> =
