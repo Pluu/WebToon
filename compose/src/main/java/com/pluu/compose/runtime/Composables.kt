c3a6c5ea5367527aec9cfb419c3715417a4f624a
pluu <pluulove@gmail.com>
2023-05-07 00:49:47 +0900

    미사용 Composable 함수 제거

--- compose/src/main/java/com/pluu/compose/runtime/Composables.kt
+++ compose/src/main/java/com/pluu/compose/runtime/Composables.kt
@@ -4,27 +4,12 @@ package com.pluu.compose.runtime
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshots.SnapshotStateList
 
 @Composable
 inline fun <T> rememberMutableStateOf(
     value: T
 ): MutableState<T> = remember {
     mutableStateOf(value)
-}
-
-@Composable
-inline fun <T> rememberMutableStateOf(
-    key1: Any,
-    crossinline calculation: @androidx.compose.runtime.DisallowComposableCalls () -> T
-): MutableState<T> = remember(key1) {
-    mutableStateOf(calculation())
-}
-
-@Composable
-inline fun <T> rememberMutableStateListOf(): SnapshotStateList<T> = remember {
-    mutableStateListOf()
 }
