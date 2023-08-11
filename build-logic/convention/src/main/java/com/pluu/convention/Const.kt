a3e3d1d79d4f3a11f029028e87723849e25fee86
pluu <pluulove@gmail.com>
2023-08-11 22:41:47 +0900

    Support, Android 14

--- build-logic/convention/src/main/java/com/pluu/convention/Const.kt
+++ build-logic/convention/src/main/java/com/pluu/convention/Const.kt
@@ -3,9 +3,9 @@ package com.pluu.convention
 import org.gradle.api.JavaVersion
 
 object Const {
-    const val compileSdk = 33
+    const val compileSdk = 34
     const val minSdk = 21
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val JDK_VERSION = 11
     val JAVA_VERSION = JavaVersion.VERSION_11
 }
