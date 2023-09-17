0ac2494b79a73f903c2804ace36c13f888334e94
pluu <pluulove@gmail.com>
2023-09-17 14:12:33 +0900

    cleanup App module dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("pluu.android.application")
-    id("pluu.android.application.compose")
     id("pluu.android.hilt")
 }
 
@@ -17,37 +16,18 @@ android {
 }
 
 dependencies {
-    implementation(projects.core)
     implementation(projects.coreAndroid)
-    implementation(projects.model)
     implementation(projects.data)
     implementation(projects.dataLocal)
     implementation(projects.dataRemote)
-    implementation(projects.domain)
-    implementation(projects.compose)
-    implementation(projects.uiCommon)
     implementation(projects.uiIntro)
     implementation(projects.uiMainContainer)
-
-    // Android UI
-    implementation(libs.androidX.browser)
-    implementation(libs.androidX.navigation.compose)
-    implementation(libs.androidX.hilt.navigation.compose)
-
-    // Compose
-    implementation(libs.coil.compose)
-    implementation(libs.accompanist.systemUi)
 
     // kotlin
     implementation(libs.kotlin.coroutine.core)
     implementation(libs.kotlin.coroutine.android)
 
     implementation(libs.timber)
-    implementation(libs.coil.core)
-
-    testImplementation(libs.junit)
-    testImplementation(libs.assertJ)
-    testImplementation(libs.mockito)
 }
 
 kapt {
