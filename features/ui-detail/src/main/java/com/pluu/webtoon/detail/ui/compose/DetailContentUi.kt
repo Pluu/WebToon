b6c6c5881d6286af8917358bfc21ea3eba8c82a8
pluu <pluulove@gmail.com>
2024-04-20 15:34:15 +0900

    상세 화면 마이그레이션

--- features/ui-detail/src/main/java/com/pluu/webtoon/detail/ui/compose/DetailContentUi.kt
+++ features/ui-detail/src/main/java/com/pluu/webtoon/detail/ui/compose/DetailContentUi.kt
@@ -12,16 +12,18 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
 import com.pluu.webtoon.model.DetailView
-import com.pluu.webtoon.utils.applyAgent
+import com.pluu.webtoon.ui.compose.theme.themeRed
+import com.pluu.webtoon.ui_common.R
+import com.pluu.webtoon.utils.glideUrl
+import com.skydoves.landscapist.glide.GlideImage
+import com.skydoves.landscapist.glide.GlideImageState
 
 @Composable
 internal fun DetailContentUi(
@@ -65,32 +67,27 @@ private fun AdjustDetailImage(
     modifier: Modifier = Modifier,
     onSuccess: (DetailView, Size) -> Unit
 ) {
-    val painter = rememberAsyncImagePainter(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data(item.url)
-            .applyAgent()
-            .crossfade(true)
-            .build()
-    )
-
-    when (val state = painter.state) {
-        is AsyncImagePainter.State.Success -> {
-            onSuccess(item, state.painter.intrinsicSize)
-        }
-
-        is AsyncImagePainter.State.Loading -> {
+    GlideImage(
+        imageModel = { item.url.glideUrl() },
+        modifier = modifier,
+        loading = {
             CircularProgressIndicator(
                 modifier = modifier.size(60.dp),
-                color = com.pluu.webtoon.ui.compose.theme.themeRed
+                color = themeRed
             )
+        },
+        failure = {
+            Image(
+                modifier = Modifier.align(Alignment.Center),
+                painter = painterResource(R.drawable.ic_sentiment_very_dissatisfied_48),
+                contentDescription = null
+            )
+        },
+        onImageStateChanged = {
+            if (it is GlideImageState.Success) {
+                val bitmap = it.imageBitmap!!
+                onSuccess(item, Size(bitmap.width.toFloat(), bitmap.height.toFloat()))
+            }
         }
-
-        else -> {}
-    }
-
-    Image(
-        modifier = modifier,
-        painter = painter,
-        contentDescription = null
     )
 }
