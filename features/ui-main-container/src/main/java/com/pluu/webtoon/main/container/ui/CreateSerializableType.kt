256ba7e6020eec8bf9314d287049ea813dc3f845
pluu <pluulove@gmail.com>
2024-08-07 22:41:32 +0900

    Serializable 정의 수정

--- features/ui-main-container/src/main/java/com/pluu/webtoon/main/container/ui/CreateSerializableType.kt
+++ features/ui-main-container/src/main/java/com/pluu/webtoon/main/container/ui/CreateSerializableType.kt
@@ -1,8 +1,9 @@
 package com.pluu.webtoon.main.container.ui
 
 import androidx.navigation.NavType
+import java.io.Serializable
 
-class SerializableType<T : java.io.Serializable>(
+class SerializableType<T : Serializable>(
     type: Class<T>,
     private val parser: (String) -> T
 ) : NavType.SerializableType<T>(type) {
