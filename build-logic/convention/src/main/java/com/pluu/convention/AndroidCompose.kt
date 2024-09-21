1c188d05ac45338506b06121b14f6c3af5129993
pluu <pluulove@gmail.com>
2024-09-21 11:22:54 +0900

    Update, Compose metrics/stability settings

--- build-logic/convention/src/main/java/com/pluu/convention/AndroidCompose.kt
+++ build-logic/convention/src/main/java/com/pluu/convention/AndroidCompose.kt
@@ -5,6 +5,7 @@ package com.pluu.convention
 ///////////////////////////////////////////////////////////////////////////
 
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.withType
@@ -21,8 +22,6 @@ internal fun Project.configureAndroidCompose(
         buildFeatures.compose = true
     }
 
-    configureComposeCompiler()
-
     dependencies {
         // Disabling to work with Alpha
         api(platform(libs.findLibrary("androidX-compose-bom").get()))
@@ -32,7 +31,6 @@ internal fun Project.configureAndroidCompose(
 
     tasks.withType<KotlinCompile>().configureEach {
         compilerOptions {
-            freeCompilerArgs.addAll(buildComposeMetricsParameters())
             freeCompilerArgs.addAll(
                 listOf(
                     "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
@@ -41,37 +39,23 @@ internal fun Project.configureAndroidCompose(
             )
         }
     }
-}
 
-private fun Project.configureComposeCompiler() {
     extensions.configure<ComposeCompilerGradlePluginExtension> {
-        // StrongSkipping은 기본 활성화 상태
-//        featureFlags.add(ComposeFeatureFlag.StrongSkipping)
-    }
-}
+        fun Provider<String>.onlyIfTrue() = flatMap { provider { it.takeIf(String::toBoolean) } }
+        fun Provider<*>.relativeToRootProject(dir: String) = flatMap {
+            rootProject.layout.buildDirectory.dir(projectDir.toRelativeString(rootDir))
+        }.map { it.dir(dir) }
 
-private fun Project.buildComposeMetricsParameters(): List<String> {
-    val metricParameters = mutableListOf<String>()
-    val enableMetricsProvider = project.providers.gradleProperty("enableComposeCompilerMetrics")
-    val relativePath = projectDir.relativeTo(rootDir)
-    val buildDir = layout.buildDirectory.get().asFile
-    val enableMetrics = (enableMetricsProvider.orNull == "true")
-    if (enableMetrics) {
-        val metricsFolder = buildDir.resolve("compose-metrics").resolve(relativePath)
-        metricParameters.add("-P")
-        metricParameters.add(
-            "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + metricsFolder.absolutePath
+        project.providers.gradleProperty("enableComposeCompilerMetrics").onlyIfTrue()
+            .relativeToRootProject("compose-metrics")
+            .let(metricsDestination::set)
+
+        project.providers.gradleProperty("enableComposeCompilerReports").onlyIfTrue()
+            .relativeToRootProject("compose-reports")
+            .let(reportsDestination::set)
+
+        stabilityConfigurationFile.set(
+            rootProject.layout.projectDirectory.file("compose_compiler_config.conf")
         )
     }
-
-    val enableReportsProvider = project.providers.gradleProperty("enableComposeCompilerReports")
-    val enableReports = (enableReportsProvider.orNull == "true")
-    if (enableReports) {
-        val reportsFolder = buildDir.resolve("compose-reports").resolve(relativePath)
-        metricParameters.add("-P")
-        metricParameters.add(
-            "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + reportsFolder.absolutePath
-        )
-    }
-    return metricParameters.toList()
 }
