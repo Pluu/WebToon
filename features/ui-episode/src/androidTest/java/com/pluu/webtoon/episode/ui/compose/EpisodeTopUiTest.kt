8ce0082687fcc25b1776c787a0a94951e4eb476e
pluu <pluulove@gmail.com>
2024-12-27 01:19:46 +0900

    Basic EpisodeTopUi Composable test

---
+++ features/ui-episode/src/androidTest/java/com/pluu/webtoon/episode/ui/compose/EpisodeTopUiTest.kt
@@ -0,0 +1,31 @@
+package com.pluu.webtoon.episode.ui.compose
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+class EpisodeTopUiTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun t1() {
+        composeTestRule.setContent {
+            EpisodeTopUi(
+                title = "Test",
+                backgroundColor = MaterialTheme.colorScheme.background,
+                contentColor = MaterialTheme.colorScheme.onBackground,
+                isFavorite = false,
+                onBackPressed = {},
+                onFavoriteClicked = {}
+            )
+        }
+
+        composeTestRule
+            .onNodeWithText("Test")
+            .assertExists()
+    }
+}
