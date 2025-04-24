cd3ca78a1aaca0b7ea6c203fac35a7b96adedd76
pluu <pluulove@gmail.com>
2025-04-25 01:25:37 +0900

    Update, targetSdk 35

--- build-logic/convention/src/main/java/com/pluu/convention/Const.kt
+++ build-logic/convention/src/main/java/com/pluu/convention/Const.kt
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 object Const {
     const val compileSdk = 36
     const val minSdk = 21
-    const val targetSdk = 34
+    const val targetSdk = 35
     val JAVA_VERSION = JavaVersion.VERSION_11
     val JVM_TARGET = JvmTarget.JVM_11
 }
