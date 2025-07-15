7d22b53217fa3b1ee9a47f83971ac3fa6c9c94bb
pluu <pluulove@gmail.com>
2025-07-15 23:44:34 +0900

    Update, Gradle 9.0.0-rc-2

--- build-logic/convention/src/main/java/com/pluu/convention/DependencyHandlerExt.kt
+++ build-logic/convention/src/main/java/com/pluu/convention/DependencyHandlerExt.kt
@@ -5,18 +5,18 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.provider.Provider
 import java.util.Optional
 
-internal fun <T> DependencyHandler.implementation(
+internal fun <T : Any> DependencyHandler.implementation(
     dependencyNotation: Optional<Provider<T>>
 ): Dependency? = add("implementation", dependencyNotation.get())
 
-internal fun <T> DependencyHandler.debugImplementation(
+internal fun <T : Any> DependencyHandler.debugImplementation(
     dependencyNotation: Optional<Provider<T>>
 ): Dependency? = add("debugImplementation", dependencyNotation.get())
 
-internal fun <T> DependencyHandler.ksp(
+internal fun <T : Any> DependencyHandler.ksp(
     dependencyNotation: Optional<Provider<T>>
 ): Dependency? = add("ksp", dependencyNotation.get())
 
-internal fun <T> DependencyHandler.api(
+internal fun <T : Any> DependencyHandler.api(
     dependencyNotation: Provider<T>
 ): Dependency? = add("api", dependencyNotation)
