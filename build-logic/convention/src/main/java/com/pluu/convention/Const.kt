ea546350aca14fb727f13f212ee96ce40e4f20a2
pluu <pluulove@gmail.com>
2025-08-15 21:58:14 +0900

    Update, AndroidX August 13, 2025

--- build-logic/convention/src/main/java/com/pluu/convention/Const.kt
+++ build-logic/convention/src/main/java/com/pluu/convention/Const.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 object Const {
     const val compileSdk = 36
-    const val minSdk = 21
+    const val minSdk = 23
     const val targetSdk = 36
     val JAVA_VERSION = JavaVersion.VERSION_11
     val JVM_TARGET = JvmTarget.JVM_11
