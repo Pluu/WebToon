be2836ff455231492a71eaaa972349c12de3c2cd
pluu <pluulove@gmail.com>
2026-03-07 14:40:46 +0900

    Update, settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
 
 plugins {
     id("com.gradle.develocity").version("4.1.1")
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "WebToon"
