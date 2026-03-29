4c6558d72d904de03ab4dede32704856f0b3b84a
pluu <pluulove@gmail.com>
2026-03-29 22:31:17 +0900

    Cleanup, kotlin-metadata-jvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,15 +16,6 @@ subprojects {
     project.apply {
         plugin("versions.checker")
     }
-
-    // TODO: https://github.com/google/dagger/issues/5001
-    configurations.configureEach {
-        resolutionStrategy.eachDependency {
-            if (requested.name.startsWith("kotlin-metadata-jvm")) {
-                useVersion(libs.versions.kotlin.asProvider().get())
-            }
-        }
-    }
 }
 
 tasks.register("clean", Delete::class) {
