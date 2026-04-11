899691040e7d49c84372f80309b74f4761bd215d
pluu <pluulove@gmail.com>
2026-04-12 00:05:52 +0900

    Update, VersionCatalog Loader

--- build-logic/convention/src/main/java/com/pluu/convention/ProjectExt.kt
+++ build-logic/convention/src/main/java/com/pluu/convention/ProjectExt.kt
@@ -1,9 +1,13 @@
 package com.pluu.convention
 
+///////////////////////////////////////////////////////////////////////////
+// Origin : https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:buildSrc/public/src/main/kotlin/androidx/build/VersionCatalogExtensions.kt
+///////////////////////////////////////////////////////////////////////////
+
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
 
-val Project.libs
-    get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+val Project.libs: VersionCatalog
+    get() = project.extensions.getByType<VersionCatalogsExtension>().named("libs")
